de6d9f6655372304178935254e6d86fe7a897114
Omer El Idrissi <omer.e.idrissi@gmail.com>
2026-04-07 20:36:01 +0200

    Fix function return type

--- core.h
+++ core.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <clang-c/Index.h>
 
+#define DEFAULT_NUM_CURSORS 5
+
 struct prong_priv {
 	char **func_names;
 	int num_funcs;
@@ -15,7 +17,7 @@ CXTranslationUnit	*alloc_tu_array(int length);
 CXCursor		*alloc_cursor_array(int length);
 void process_tu_array(CXTranslationUnit *tu_array, int length, void *client_data);
 struct prong_priv *prong_init_priv(char **argv, int funcs_pos, int num_funcs);
-void *prong_free_priv(struct prong_priv *prong_priv);
+void prong_free_priv(struct prong_priv *prong_priv);
 
 void process_args(int argc, char **argv, 
 		  int *files_pos, int *funcs_pos,
